7c320d61ac28172c6a2180dba41aab7801efe637
parsecer <firebugers@gmail.com>
2015-04-19 21:13:01 +0300

    Update start.py

--- start.py
+++ start.py
@@ -5,21 +5,51 @@ from random import *
 A=[12,16,4,18,3]
 print A
 # Сортировка списка методом пузырька (без использования языковых конструкций типа sorted)
-z=0
-while z!=1:
-    for x in range(len(A)-1):
-        if A[x]>A[x+1]:
-            A[x],A[x+1]=A[x+1],A[x]
-            break
-    else:
-        z=1
-print A
+def list_sorted(l):
+    """
+    >>> list_sorted([2,5,3,1])
+    [1, 2, 3, 5]
+    >>> 
+    """
+    z=0
+    while z!=1:
+        for x in range(len(l)-1):
+            if l[x]>l[x+1]:
+                l[x],l[x+1]=l[x+1],l[x]
+                break
+        else:
+            z=1
+    print l
+list_sorted(A)
 # Создание из списка словаря вида {3: 0, 5: 1, 1: 2, 6: 3, 7: 4}, где значениями словаря являются индексы ключей списка (HINT: использовать enumerate)
-n=list(enumerate(A))
-f={}
-for x,y in n:
-    f[y]=x
+
+def list_to_dict(l):
+    """
+    >>> list_to_dict([1,2,3])
+    {1: 0, 2: 1, 3: 2}
+    >>>
+    """
+    f={}
+    n=list(enumerate(l))
+    for x,y in n:
+        f[y]=x
+    return f
+f=list_to_dict(A)
 print f
-# Смена у словаря местами ключей и значений. 
-f={y:x for x,y in f.items()}   
-print f
+    # Смена у словаря местами ключей и значений. 
+def change_dict(d): 
+    """
+    >>> change_dict({1:'one',2:'two',3:'three'})
+    {'three': 3, 'two': 2, 'one': 1}
+    >>>
+    """
+    if type(d)==dict:   
+        d={y:x for x,y in d.items()}   
+        print d
+    else:
+        print 'Give me a dictionary'
+        print d, type(d)
+change_dict(f)    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
